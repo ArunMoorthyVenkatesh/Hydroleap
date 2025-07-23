a85c9035fe79c9227b1cb5cdc8f8a212ad8fcc87
Arun Moorthy Venkatesh <arunmoorthyvenkatesh@Aruns-MacBook-Pro.local>
2025-07-23 13:18:54 +0800

    Migrated from old repo folder

--- hydroleap-backend/server.js
+++ hydroleap-backend/server.js
@@ -115,6 +115,7 @@ mongoose
 
 // --- Start Server ---
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
